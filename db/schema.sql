ace17bda3ce8228ab087a0533bdfd97c7c1dd0f5
Steffan Carter <xisled09@gmail.com>
2022-08-02 08:53:00 -0400

    added to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,28 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
+-- creates new blank database
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
+USE employee_trackerDB;
 
-USE employee_db;
-
+-- creates tables for department, roles and employees
 CREATE TABLE department (
-	id INT NOT NULL,
-    department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  names VARCHAR(30) 
 );
-    
-CREATe TABLE role (
-	id INT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    PRIMARY KEY (id)
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-	id INT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
